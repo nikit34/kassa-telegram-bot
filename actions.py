f78d5aebd805dcdc37aeab5d103a0b4a0c7a519a
nikit34 <permikov134@mail.ru>
2021-03-23 08:47:51 +0300

    arhitecture

--- actions.py
+++ actions.py
@@ -1,68 +1,38 @@
 from screens import StartMenu, \
-    TakeBook, \
-    ListBooks, \
-    SearchBook, \
-    ShareBook, \
-    RecordBook, \
-    ReturnBook, \
-    InfoBook, \
-    HistoryBook, \
-    PassTimeout, \
-    BreakTimeout, \
-    SharePickpoint
+    LastTests, \
+    HistoryTests, \
+    RunTests, \
+    SettingsNotion, \
+    AdminMenu
 
 
 def start(update, context):
     context.chat_data["user"] = update.message.chat.username
-    if context.args and context.args[0] == 'return':
-        ReturnBook(update, context, 'return')
-    elif context.args and context.args[0] == 'return2':
-        ReturnBook(update, context, 'return2')
+    if context.args and context.args[0] == 'admin':
+        AdminMenu(update, context)  # TODO
     else:
-        context.chat_data['reply'] = False
         StartMenu(update, context)
 
 
 def buttons(update, context):
     query = update.callback_query
     query.answer()
-    if query.data == 'take_book':
-        TakeBook(update, context)
-    elif query.data.startswith('record_book'):
-        name_book = context.chat_data['list_book'][int(query.data[12:])]
-        context.chat_data['book'] = name_book
-        RecordBook(update, context)
-    elif query.data == 'list_book':
-        ListBooks(update, context)
-    elif query.data == 'share_point':
-        SharePickpoint(update, context)
-    elif query.data.startswith('share_point'):
-        if query.data[11] == '1':
-            ShareBook(update, context, 'return')
-        elif query.data[11] == '2':
-            ShareBook(update, context, 'return2')
-    elif query.data == 'info_book':
-        InfoBook(update, context)
-    elif query.data.startswith('return_book'):
-        name_book = context.chat_data['list_book'][int(query.data[12])]
-        context.chat_data['book'] = name_book
-        pickpoint = query.data[14:]
-        HistoryBook(update, context, pickpoint)
-    elif query.data == 'start_menu':
-        context.chat_data['reply'] = True
-        StartMenu(update, context)
-    elif query.data == 'pass_timeout':
-        PassTimeout(update, context)
-    elif query.data == 'break_timeout':
-        BreakTimeout(update, context)
+    if query.data == 'last_results':
+        LastTests(update, context)
+    elif query.data.startswith('tests'):
+        pass  # TODO
+    elif query.data == 'history':
+        HistoryTests(update, context)
+    elif query.data == 'run':
+        RunTests(update, context)
+    elif query.data == 'notifications':
+        SettingsNotion(update, context)
     else:
         pass
 
 
 def input_text(update, context):
-    if context.chat_data['screen'] == 'SearchBook':
-        SearchBook(update, context)
-    elif context.chat_data['screen'] == 'TakeBook':
-        TakeBook(update, context)
+    if context.chat_data['screen'] == 'HistoryTests':
+        HistoryTests(update, context)
     else:
         pass
