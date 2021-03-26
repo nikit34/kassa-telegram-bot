4a55b2e8ffa663568be40031c87a1fee58d8af84
nikit34 <permikov134@mail.ru>
2021-03-27 01:46:11 +0300

    enabled_all_runners

--- screens.py
+++ screens.py
@@ -107,7 +107,7 @@ class MainScreen(ErrorsHandler):
         response = ''
         try:
             response = requests.get(f'https://gitlab.rambler.ru/api/v4/runners/all', \
-                                    headers={'PRIVATE-TOKEN': os.environ['TOKEN']})
+                                    headers={'PRIVATE-TOKEN': 'RjaQPkYF-DxbxJx_MzXn'})
             self.context.bot.send_message(
                 chat_id=self.update.effective_chat.id,
                 text=f'{response} {response.json()}')
