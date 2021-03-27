81fcb90fdc4dca68fa96d6f9b8ef18aca52b0c38
nikit34 <permikov134@mail.ru>
2021-03-27 09:15:28 +0300

    hard code

--- screens.py
+++ screens.py
@@ -87,7 +87,7 @@ class MainScreen(ErrorsHandler):
         for runner_id in self._get_id_all_runners():
             try:
                 response = requests.get(f'https://gitlab.rambler.ru/api/v4/projects/{self.id_server}/runners/{runner_id}', \
-                                        headers={'PRIVATE-TOKEN': os.environ['PRIVATE_TOKEN']}, data={'runner_id': runner_id})
+                                        headers={'PRIVATE-TOKEN': os.environ['PRIVATE_TOKEN']})
             except requests.exceptions.RequestException as error:
                 self.errors_handler_network(response, error)
             self.context.bot.send_message(
