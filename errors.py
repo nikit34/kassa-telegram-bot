ea473dba19f1c2dfac065ec1007c9b4435683165
nikit34 <permikov134@mail.ru>
2021-03-27 22:55:08 +0300

    emoji

--- errors.py
+++ errors.py
@@ -7,10 +7,10 @@ class ErrorsHandler:
         print(f'[ERROR] {self.__class__.__name__} -> {error.__class__}', error)
         self.context.bot.send_message(
             chat_id=self.update.effective_chat.id,
-            text=f'Error on GitLab side\n{error.__class__}')
+            text=f'❌ Error on GitLab side\n{error.__class__}')
 
     def errors_handler_network(self, response, error):
         print(f'[ERROR] {self.__class__.__name__} -> {response.request.method} failed\nstatus code: {response.status_code}', error)
         self.context.bot.send_message(
             chat_id=self.update.effective_chat.id,
-            text=f'Error on GitLab side\nin {self.__class__.__name__}\nstatus code: {response.status_code}')
+            text=f'❌ Error on GitLab side\nin {self.__class__.__name__}\nstatus code: {response.status_code}')
